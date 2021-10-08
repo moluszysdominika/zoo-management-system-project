c5aa0c420176048acd929567b72fc1bfd345737a
moluszysdominika <56396181+moluszysdominika@users.noreply.github.com>
2021-10-08 11:55:22 +0200

    Added static methods to species classes

--- zoo.php
+++ zoo.php
@@ -112,21 +112,41 @@ class Cat extends BaseAnimal
 {
     protected string $call = "Meow!";
 
+    public static function getSpecies(): string
+    {
+        return "cat";
+    }
+
 }
 
 class Dog extends BaseAnimal
 {
     protected string $call = "Woof!";
+
+    public static function getSpecies(): string
+    {
+        return "dog";
+    }
 }
 
 class Boar extends BaseAnimal
 {
     protected string $call = "Oink!";
+
+    public static function getSpecies(): string
+    {
+        return "boar";
+    }
 }
 
 class Employee extends BaseAnimal
 {
     protected string $call = "Wreee!";
+
+    public static function getSpecies(): string
+    {
+        return "human";
+    }
 }
 
 class SummedWeightCalculator implements ISummedWeightCalculator
@@ -147,7 +167,7 @@ class SummedWeightCalculator implements ISummedWeightCalculator
         $summedWeight = 0;
 
         foreach ($animals as $animal) {
-            if ($animal->getChip()->getSpecies() !== "human") {
+            if ($animal->getChip()->getSpecies() !== Employee::getSpecies()) {
                 $summedWeight += $animal->getWeight();
             }
         }
@@ -340,19 +360,19 @@ $amountOfFeedCalculator = new AmountOfFeedCalculator($summedWeightCalculator);
 $zooService = new Zoo($animalInformationDisplay, $amountOfFeedCalculator, $summedWeightCalculator);
 
 
-$cat1Chip = new Chip("NA-1234", "Nami", new DateTime("2015-07-23 15:00:00"), "cat");
+$cat1Chip = new Chip("NA-1234", "Nami", new DateTime("2015-07-23 15:00:00"), Cat::getSpecies());
 $cat1 = new Cat(3);
 
-$cat2Chip = new Chip("RA-1234", "Nami", new DateTime("2017-11-05 17:25:00"), "cat");
+$cat2Chip = new Chip("RA-1234", "Nami", new DateTime("2017-11-05 17:25:00"), Cat::getSpecies());
 $cat2 = new Cat(3);
 
-$dog1Chip = new Chip("DA-1234", "Dami", new DateTime("2020-01-17 23:57:00"), "dog");
+$dog1Chip = new Chip("DA-1234", "Dami", new DateTime("2020-01-17 23:57:00"), Dog::getSpecies());
 $dog1 = new Dog(3);
 
-$boar1Chip = new Chip("SA-1234", "Sami", new DateTime("2016-06-15 18:20:40"), "boar");
+$boar1Chip = new Chip("SA-1234", "Sami", new DateTime("2016-06-15 18:20:40"), Boar::getSpecies());
 $boar1 = new Boar(3);
 
-$employee1Chip = new Chip("AN-1234", "Anna Nowak", new DateTime("1995-03-04 10:23:42"), "human");
+$employee1Chip = new Chip("AN-1234", "Anna Nowak", new DateTime("1995-03-04 10:23:42"), Employee::getSpecies());
 $employee1 = new Employee(52);
 
 
@@ -361,9 +381,9 @@ $zooService->addAnimal($cat2, $cat2Chip);
 $zooService->addAnimal($dog1, $dog1Chip);
 $zooService->addAnimal($boar1, $boar1Chip);
 $zooService->addAnimal($employee1, $employee1Chip);
-//$zooService->removeAnimal("RA-1234");
-//var_dump($zooService->showAllAnimals());
-//var_dump($zooService->showAllAnimalsBySpecies("kot"));
+$zooService->removeAnimal("RA-1234");
+var_dump($zooService->showAllAnimals());
+//var_dump($zooService->showAllAnimalsBySpecies(Cat::getSpecies()));
 //var_dump($zooService->showAnimal($cat1Chip->getIdNumber()));
 //var_dump($zooService->getAllAnimalsSummedWeight());
-var_dump($zooService->calculateAmountOfFeedForAnimals(12));
+//var_dump($zooService->calculateAmountOfFeedForAnimals(12));
